eac893a0ee389c0f206cc0d40ff978e951998cd2
Ayush Kushwaha <ayushkushwaha855@gmail.com>
2024-03-04 22:41:37 +0530

    update main.ps1

--- Files older than 5 days/main.ps1
+++ Files older than 5 days/main.ps1
@@ -1,1 +1,31 @@
+$remoteServer = ""
+$remoteCredential = Get-Credential
 
+$scriptBlock = {
+
+$csvPath = "H:\Script\csv\csv.csv"
+
+$EmailTo = @("ayush@gmail.com")
+$emailsubject = "CSV file"
+$EmailFrom = "ayush@gmail.com"
+$SMTPServer = ""
+
+$filteredFiles = Get-ChildItem -Path H:\ | Where-Object { (Get-Date) - $_.LastWriteTime -lt (New-TimeSpan -Days 5) } 
+$filteredFiles | Export-Csv -Path $csvPath -NoTypeInformation 
+
+$body1 = "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+This is an automatic email for script execution. Kindly do not respond to this mail.
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Hi Team,
+Script: file_5days_old.ps1
+Executed by: $env:USERNAME
+Executed on: $env:COMPUTERNAME
+
+Please find the attached  report.
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+Send-MailMessage -To $EmailTo -From $EmailFrom -Subject $emailSubject -SmtpServer $SMTPServer -body $body1 -Attachments $csvPath
+
+}
+
+Invoke-Command -ComputerName $remoteServer -Credential $remoteCredential -ScriptBlock $scriptBlock
